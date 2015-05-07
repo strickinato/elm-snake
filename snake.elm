44d2f97cff4794709ec0b2d8fc484ec0583b8fa8
Aaron Strick <aaron@noredink.com>
2015-05-07 02:29:50 -0700

    :art: refactor

--- snake.elm
+++ snake.elm
@@ -10,18 +10,23 @@ import List
 import Array exposing (..)
 
 -----------------------------------------------------------
---------- CONFIG -----------------------------------------
+--------- CONFIG ------------------------------------------
 -----------------------------------------------------------
-gameSize = 1000    -- total gamesize squared
-pixels   = 3     -- pixels in game
-gameSpeed = 40    -- updates per second
-startingLength = 100
+boardSize        = 1000
+scale            = 3
+gameSpeed        = 40
+startingLength   = 100
+
+boardColor       = Color.rgb 0 0 0
+snakeColor       = Color.rgb 255 0 255
+
 
 -----------------------------------------------------------
 --------- SIGNALS -----------------------------------------
 -----------------------------------------------------------
-delta =
-  Signal.map inSeconds (fps gameSpeed)
+main =
+  Signal.map2 view Window.dimensions gameState
+
 
 input : Signal Input
 input =
@@ -29,13 +34,10 @@ input =
     (Signal.map Dir Keyboard.arrows)
     (Signal.map Delta delta)
 
-type Input
-  = Dir Direction
-  | Delta Float
 
-
-main =
-  Signal.map2 view Window.dimensions gameState
+delta : Signal Float
+delta =
+  Signal.map inSeconds (fps gameSpeed)
 
 
 -----------------------------------------------------------
@@ -45,6 +47,9 @@ gameState : Signal GameState
 gameState =
   Signal.foldp stepGame defaultGame input
 
+type Input
+  = Dir Direction
+  | Delta Float
 
 type alias Direction = { x : Int, y : Int }
 
@@ -54,24 +59,27 @@ type alias Snake =
   {direction:Direction, parts:List(Square)}
 
 type alias GameState =
-  {snake:Snake, gameSize:Int}
+  {snake:Snake, boardSize:Float}
 
 
+defaultSnake : Snake
 defaultSnake =
   { direction = { x = 1, y = 0 }
   , parts = defaultParts startingLength
   }
 
+
 defaultParts : Int -> List Square
 defaultParts length =
-  if | length == 0 -> []
-     | otherwise ->
-        (length - 1 - startingLength, 0) :: defaultParts (length - 1)
+  if length == 0
+    then []
+    else (length - 1 - startingLength, 0) :: defaultParts (length - 1)
+
 
 defaultGame : GameState
 defaultGame =
   { snake = defaultSnake
-  , gameSize = gameSize
+  , boardSize = boardSize
   }
 
 
@@ -79,27 +87,29 @@ defaultGame =
 --------- UPDATE ------------------------------------------
 -----------------------------------------------------------
 stepGame : Input -> GameState -> GameState
-stepGame input ({snake, gameSize} as gameState) =
+stepGame input ({snake, boardSize} as gameState) =
   let
     newSnake = updateSnake input snake
   in
     { gameState |
         snake <- newSnake
-      , gameSize <- gameSize
+      , boardSize <- boardSize
     }
+
 
 updateSnake : Input -> Snake -> Snake
 updateSnake input snake =
   case input of
     Dir direction ->
-      let newDirection =
-        if direction == { x = 0, y = 0 }
-          then snake.direction
-          else direction
+      let
+        newDirection =
+          if direction == { x = 0, y = 0 }
+            then snake.direction
+            else direction
       in
         { snake | direction <- newDirection }
 
-    Delta float ->
+    Delta _ ->
       let
         newParts = updateParts snake.direction snake.parts
       in
@@ -124,22 +134,39 @@ step direction square =
 --------- DISPLAY -----------------------------------------
 -----------------------------------------------------------
 view : (Int,Int) -> GameState -> Element
-view (w,h) {snake, gameSize} =
+view (w,h) {snake, boardSize} =
   collage w h
-      ((filled (Color.rgb 0 0 0) (square (toFloat (gameSize)))) :: (renderSnake snake))
+      <| renderBoard boardSize :: renderSnake snake
+
+
+renderBoard : Float -> Form
+renderBoard size =
+  filled boardColor
+    <| square size
+
 
 renderSnake : Snake -> List Form
 renderSnake {direction, parts} =
   List.map placeSegment parts
 
+
 placeSegment : Square -> Form
 placeSegment (x, y) =
-  move ((toFloat (x * pixels)), (toFloat (y * pixels))) (filled (Color.rgb 255 0 255) (square (toFloat (pixels))))
+  move (scaled x, scaled y)
+    <| filled snakeColor
+    <| square (scaled 1)
+
+
 -----------------------------------------------------------
 --------- UTILITY -----------------------------------------
 -----------------------------------------------------------
-init : List(Square) -> List(Square)
+init : List Square -> List Square
 init squares =
   case squares of
     square :: []      -> []
     square :: squares -> square :: init squares
+
+scaled : Int -> Float
+scaled x =
+  toFloat (x * scale)
+
